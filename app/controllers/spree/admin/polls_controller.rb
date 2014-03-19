d2f475c30aaa94cbd01f5b22a6f0792319fb9d1f
arturro <artchru@gmail.com>
2014-03-19 12:45:37 +0530

    build anwers

--- app/controllers/spree/admin/polls_controller.rb
+++ app/controllers/spree/admin/polls_controller.rb
@@ -2,6 +2,8 @@ module Spree
   module Admin
     class PollsController < ResourceController
 
+      before_filter :build_answers, only: :new 
+
       def index
         session[:return_to] = request.url
         respond_with(@collection)
@@ -13,6 +15,10 @@ module Spree
       end
 
       private
+        def build_answers
+          2.times { @poll.answers.build }
+        end
+
         def collection
           return @collection if @collection.present?
           params[:q] ||= {}
@@ -25,6 +31,10 @@ module Spree
             page(params[:page]).
             per(Spree::Suffrage::Config[:admin_polls_per_page])
           @collection
+        end
+
+        def permitted_resource_params
+          params.require(:poll).permit(:name, :question, :allow_view_results_without_voting, poll_answer_attributes: [:answer])
         end
     end
   end
