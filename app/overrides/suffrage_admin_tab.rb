724d819e4f04b12e91ae0f3d7516f387d362356d
arturro <artchru@gmail.com>
2014-03-19 11:39:13 +0530

    its a partial

--- app/overrides/suffrage_admin_tab.rb
+++ app/overrides/suffrage_admin_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "suffrage_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :text => "<%= tab(:polls, :icon => 'icon-question-sign') %>")
