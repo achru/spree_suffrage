99e35a11c58f987684fe0fb6bcec310ba6c73aa6
arturro <artchru@gmail.com>
2014-03-20 11:35:36 +0530

    typo

--- app/overrides/suffrage_admin_tab.rb
+++ app/overrides/suffrage_admin_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configurations_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "suffrage_admin_tab",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
                      :text => "<%= configurations_sidebar_menu_item Spree.t(:polls), admin_polls_path %>"
