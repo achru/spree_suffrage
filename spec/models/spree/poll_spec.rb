087bf325641b33983bb2212bb892e526c9fbc8aa
Piotr Pałek <piotr.palek@netguru.pl>
2014-06-02 16:28:11 +0200

    fix poll model spec [#72428532]

--- spec/models/spree/poll_spec.rb
+++ spec/models/spree/poll_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe Spree::Poll do
+  let(:poll) { create(:poll) }
+
   context "#create" do
     it "should create two default poll answers" do
-      poll = Spree::Poll.create(name: 'foo', question: 'bar')
       poll.poll_answers.count.should == 2 # yes & no
     end
   end
+
   context "#update" do
     it "should ensure at two default poll answers" do
-      poll = Spree::Poll.create!(name: 'foo', question: 'bar')
       poll.update_attributes(name: 'zoo')
       poll.poll_answers.count.should >= 2 # at least yes & no
     end
   end
+
   context "#destroy" do
     it "should clean up the answers upon removal" do
-      poll = Spree::Poll.create!(name: 'foo', question: 'bar')
       answers = poll.poll_answers
       poll.destroy
       answers.each do |answer|
