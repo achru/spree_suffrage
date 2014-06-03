35f0abfd6c896e8047adaeaf46726904ae7d63f3
Piotr Pałek <piotr.palek@netguru.pl>
2014-06-03 16:48:24 +0200

    use vote helper method in shared context [#72428532]

--- spec/support/shared_contexts/check_voting_allowed.rb
+++ spec/support/shared_contexts/check_voting_allowed.rb
@@ -1,10 +1,9 @@
+require 'support/helper_methods'
+
 shared_context "check_voting_allowed" do
   def check_voting_allowed
    page.should have_selector("#new_poll_list")
-
-   page.find("input[type=radio]").click
-   click_button I18n.t(:vote)
-
+   vote!
    page.should have_content(I18n.t(:thanks_for_voting))
    page.should have_selector("#poll_results_list")
   end
