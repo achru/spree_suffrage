8fcb935eda44080e853959f6ae83c56a7f80a0aa
Piotr Pałek <piotr.palek@netguru.pl>
2014-06-03 16:48:42 +0200

    fix polls_spec [#72428532]

--- spec/features/polls_spec.rb
+++ spec/features/polls_spec.rb
@@ -3,75 +3,78 @@ require 'support/shared_contexts/check_voting_allowed'
 
 describe "Polls" do
   include_context "check_voting_allowed"
-  let!(:product) { # force the sidbare to be loaded
-    product = create(:product)
-    product.taxons << create(:taxon)
-  }
-  let!(:poll) { create(:poll, :allow_view_results_without_voting => false) }
-  let(:user) { create(:user) }
 
-  before(:each) do
-    Spree::PollVote.all.map(&:destroy)
+  before(:all) do
+    Spree::User.destroy_all
+
     Deface::Override.new(name: 'add_poll_to_home_page',
                          virtual_path: 'spree/shared/_sidebar',
-                         insert_top: '#sidebar[data-hook],[data-hook=sidebar]',
+                         insert_top: '#sidebar',
                          partial: 'spree/polls/poll')
+  end
+
+  before(:each) do
+    Spree::User.destroy_all
+    Spree::Poll.destroy_all
+    # Spree::PollVote.destroy_all
+    @poll = create(:poll, :allow_view_results_without_voting => false)
+    @user = create(:user)
     visit spree.root_path
   end
 
-  context "not voted in this poll" do
-    context "as a logged in user" do
-      # can't get sign_in method for some reason
+  let!(:product) do
+    product = create(:product)
+    product.taxons << create(:taxon)
+  end
+
+  context "user has not voted in this poll" do
+    context "as a logged in user", js: true do
+      before { sign_in_as! @user }
       it "should allow me to vote" do
-        sign_in user
-
         check_voting_allowed
       end
     end
+
     context "as an anonymous user" do
-      it "should allow me to vote", js: true do # no idea why this won't work w/out js: true
+      it "should allow me to vote", js: true do
         check_voting_allowed
       end
     end
 
-    context "view poll results before voting" do
-      context "viewing results is allowed for poll" do
-        before do
-          poll.update_attributes(allow_view_results_without_voting: true)
-        end
+    context "viewing results is allowed for poll" do
+      before { @poll.update_attributes(allow_view_results_without_voting: true) }
 
-        it "should not show the 'view results' link if there hasn't been any voting yet" do
-          visit spree.root_path
+      describe "hasn't been voted yet" do
+        before { visit spree.root_path }
 
+        it "should not show the 'view results' link" do
           page.should_not have_link(I18n.t(:view_poll_results))
         end
+      end
+
+      describe "someone has voted" do
+        let!(:poll_vote_1) { create(:poll_vote, poll_answer: @poll.poll_answers.first) }
+        before { visit spree.root_path }
 
         it "should allow me to see the results" do
-          vote = Spree::PollVote.new
-          vote.poll_answer_id = poll.poll_answers.first.id
-          vote.ip_address = '1.2.3.4'
-          vote.save!
-
-          visit spree.root_path
-
           click_link I18n.t(:view_poll_results)
           page.should have_selector("#poll_results_list")
         end
       end
-      context "viewing results is not allowed" do
-        it "should allow me to see the results" do
-          page.should_not have_link(I18n.t(:view_poll_results))
-        end
+    end
+
+    context "viewing results is not allowed" do
+      it "should allow me to see the results" do
+        page.should_not have_link(I18n.t(:view_poll_results))
       end
     end
   end
 
   context "already voted in this poll" do
     context "logged in user" do
-      # can't get sign_in method for some reason
-      it "should show me the poll results" do
-        sign_in user
-
+      it "should show me the poll results", js: true do
+        sign_in_as! @user
+        vote!
         page.should have_selector("#poll_results_list")
       end
     end
