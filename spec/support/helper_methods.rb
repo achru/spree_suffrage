1893481011bdec79623a39e98c67d7ed449d62d3
Piotr Pałek <piotr.palek@netguru.pl>
2014-06-03 16:48:05 +0200

    add vote helper method [#72428532]

--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -4,3 +4,8 @@ def sign_in_as!(user)
   fill_in 'Password', :with => 'secret'
   click_button 'Login'
 end
+
+def vote!
+  page.find("#new_poll_list input[type=radio]").click
+  page.find("#new_poll_vote_container input[type=submit]").click
+end
